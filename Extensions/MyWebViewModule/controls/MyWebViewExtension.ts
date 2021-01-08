df67bd9e0a865cc01e43d48161c0c2bd7c2bfba9
Leonel Rojas <leonel.eduardo.rojas.aranda@everis.com>
2021-01-08 23:49:23 +0000

    se agrega plugin de utils

--- Extensions/MyWebViewModule/controls/MyWebViewExtension.ts
+++ Extensions/MyWebViewModule/controls/MyWebViewExtension.ts
@@ -12,6 +12,8 @@ import { WebView, LoadEventData } from "tns-core-modules/ui/web-view";
 import { WebViewUtils } from 'nativescript-webview-utils';
 import { request, getFile, getImage, getJSON, getString } from "tns-core-modules/http";
 import { HtmlView } from "tns-core-modules/ui/html-view";
+import { InAppBrowser } from 'nativescript-inappbrowser';
+import { Utils, Dialogs } from 'nativescript/core';
 export class MyWebViewClass extends IControl {
     private _model: any;
     private _observable: BaseObservable;
@@ -89,6 +91,61 @@ export class MyWebViewClass extends IControl {
                 });
                 this.oDonutSeries.items = this._model.getSalesOrderHeader();
             }.bind(this));
+        }
+    }
+    public async openLink() {
+        try {
+            const url = 'https://www.google.com'
+            if (await InAppBrowser.isAvailable()) {
+                const result = await InAppBrowser.open(url, {
+                    // iOS Properties
+                    dismissButtonStyle: 'cancel',
+                    preferredBarTintColor: '#453AA4',
+                    preferredControlTintColor: 'white',
+                    readerMode: false,
+                    animated: true,
+                    modalPresentationStyle: 'fullScreen',
+                    modalTransitionStyle: 'coverVertical',
+                    modalEnabled: true,
+                    enableBarCollapsing: false,
+                    // Android Properties
+                    showTitle: true,
+                    toolbarColor: '#6200EE',
+                    secondaryToolbarColor: 'black',
+                    enableUrlBarHiding: true,
+                    enableDefaultShare: true,
+                    forceCloseOnRedirection: false,
+                    // Specify full animation resource identifier(package:anim/name)
+                    // or only resource name(in case of animation bundled with app).
+                    animations: {
+                        startEnter: 'slide_in_right',
+                        startExit: 'slide_out_left',
+                        endEnter: 'slide_in_left',
+                        endExit: 'slide_out_right'
+                    },
+                    headers: {
+                        'my-custom-header': 'my custom header value'
+                    },
+                    hasBackButton: true,
+                    browserPackage: '',
+                    showInRecents: false
+                });
+                Dialogs.alert({
+                    title: 'Response',
+                    message: JSON.stringify(result),
+                    okButtonText: 'Ok'
+                });
+            }
+            else {
+                Utils.openUrl(url);
+            }
+        }
+        catch (error) {
+            Dialogs.alert({
+                title: 'Error',
+                message: error.message,
+                okButtonText: 'Ok'
+            });
         }
     }
     public webViewLoaded(args) {
