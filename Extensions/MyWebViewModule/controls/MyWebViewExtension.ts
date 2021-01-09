d9ba63913a1128fc529e3b001ae76a2fa68a73a4
Leonel Rojas <leonel.eduardo.rojas.aranda@everis.com>
2021-01-09 02:10:31 +0000

    se agrega plugin de utils

--- Extensions/MyWebViewModule/controls/MyWebViewExtension.ts
+++ Extensions/MyWebViewModule/controls/MyWebViewExtension.ts
@@ -128,61 +128,6 @@ export class MyWebViewClass extends IControl {
             }.bind(this));
         }
     }
-    public async openLink() {
-        try {
-            const url = 'https://www.google.com'
-            if (await InAppBrowser.isAvailable()) {
-                const result = await InAppBrowser.open(url, {
-                    // iOS Properties
-                    dismissButtonStyle: 'cancel',
-                    preferredBarTintColor: '#453AA4',
-                    preferredControlTintColor: 'white',
-                    readerMode: false,
-                    animated: true,
-                    modalPresentationStyle: 'fullScreen',
-                    modalTransitionStyle: 'coverVertical',
-                    modalEnabled: true,
-                    enableBarCollapsing: false,
-                    // Android Properties
-                    showTitle: true,
-                    toolbarColor: '#6200EE',
-                    secondaryToolbarColor: 'black',
-                    enableUrlBarHiding: true,
-                    enableDefaultShare: true,
-                    forceCloseOnRedirection: false,
-                    // Specify full animation resource identifier(package:anim/name)
-                    // or only resource name(in case of animation bundled with app).
-                    animations: {
-                        startEnter: 'slide_in_right',
-                        startExit: 'slide_out_left',
-                        endEnter: 'slide_in_left',
-                        endExit: 'slide_out_right'
-                    },
-                    headers: {
-                        'my-custom-header': 'my custom header value'
-                    },
-                    hasBackButton: true,
-                    browserPackage: '',
-                    showInRecents: false
-                });
-                Dialogs.alert({
-                    title: 'Response',
-                    message: JSON.stringify(result),
-                    okButtonText: 'Ok'
-                });
-            }
-            else {
-                Utils.openUrl(url);
-            }
-        }
-        catch (error) {
-            Dialogs.alert({
-                title: 'Error',
-                message: error.message,
-                okButtonText: 'Ok'
-            });
-        }
-    }
     public webViewLoaded(args) {
         const webView: WebView = <WebView>args.object;
     }
